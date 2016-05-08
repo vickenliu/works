0072b17a1f3adb83cc4bd0d4ba1fe83d933d1e86
Vicken <vicken.liu@gmail.com>
2016-05-08 23:45:43 +1200

    change image to online img

--- js/app.js
+++ js/app.js
@@ -17,49 +17,49 @@ var projects=[{
 	name:'Meowtown',
 	info:'This was a school challenge for server-side scripting, I added database and handle-bars with server redering to show my interested of this project.It covers : backend server, database, server-side rendering',
 	link:'cat-city.herokuapp.com',
-	img:'img/cat.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707560/cat_cbqfsq.png',
 	type:'personal'
 },{
 	id:5,
 	name:'Re-Reddit',
 	info:'This was originally a group full-stack project of 3 people in 2 days,I brought it furture to how it is now over the following saturday. We used react+redux ,facebook oauth log-in, backend server & database. It has all the feature of reddit.',
 	link:'re-reddit.herokuapp.com',
-	img:'img/reddit.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707580/reddit_znppoz.png',
 	type:'personal'
 },{
 	id:1,
 	name:'EDA blogs',
 	info:'Here is where my EDA study assignments go,including blogs,and js Apps. It records most of reflections and learning experience',
 	link:'vickenliu.github.io',
-	img:'img/eda.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707600/eda_ozkjfq.png',
 	type:'personal'
 },{
 	id:2,
 	name:'Tongle Health Consulting Service Center',
 	info:'Transfer an old style plain page to a modern, clean-designed website.Fully functinal, cross-browsers,and mobile friendly.',
 	link:'www.tlgay.com',
-	img:'img/tl.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707601/tl_yfubji.png',
 	type:'commercial'
 },{
 	id:4,
 	name:'Portfolio | App',
 	info:'Deliver the one page App to showcase my works, and playing around with AJAX and new designed layout. ',
 	link:'www.vickenliu.com/test',
-	img:'img/test.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707606/test_qwzob4.png',
 	type:'personal'
 },{
 	id:6,
 	name:'SHARE WITH ME',
 	info:"I had an idea to recieve others' sharing with me, I created this App with Ruby on Rails to play around and inspire peopele to contact with me.",
 	link:'share-with-me.herokuapp.com',
-	img:'img/rails.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707574/rails_qqen0j.png',
 	type:'commercial'
 },{
 	id:7,
 	name:'This is Me | App',
 	info:'Portfolio of one page App developed with Angular Js',
 	link:'/',
-	img:'img/angular.png',
+	img:'http://res.cloudinary.com/vicken/image/upload/c_scale,w_500/v1462707551/angular_dsppsv.png',
 	type:'commercial'
 }],
 types=['personal','commercial'],
